afe82889697e88ad22f8dea7537bc1177821e04e
Chris Diamand <chris@diamand.org>
2014-12-18 11:20:24 +0000

    Add libantlr3.4 directory to include path.

--- src/scripts/libcrunch_envsetup.sh
+++ src/scripts/libcrunch_envsetup.sh
@@ -32,6 +32,7 @@ export ANTLR_M4_PATH=$SRK31_PROJECTS/m4ntlr
 # http://www.antlr3.org/download/antlr-3.4.tar.gz
 
 export ANTLR_SRC_PATH=`realpath ~/src/antlr-3.4`
+add_incl_path $ANTLR_SRC_PATH/runtime/C/dist/libantlr3c-3.4
 add_incl_path $ANTLR_SRC_PATH/runtime/C/dist/libantlr3c-3.4/include
 add_link_path $ANTLR_SRC_PATH/runtime/C/dist/libantlr3c-3.4/.libs
 add_class_path $ANTLR_SRC_PATH/lib/antlr-3.4-complete.jar
