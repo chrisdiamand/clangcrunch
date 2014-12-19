0de222c5facd880361653a96e8fc953cfa13c3bd
Chris Diamand <chris@diamand.org>
2014-12-19 17:32:39 +0000

    Add instructions and packages to build with Ninja.

--- src/scripts/clang_envsetup.sh
+++ src/scripts/clang_envsetup.sh
@@ -1,4 +1,4 @@
 LLVM_SOURCE=`realpath ~/src/llvm`
 
-add_bin $LLVM_SOURCE/build/Debug+Asserts/bin
-add_lib $LLVM_SOURCE/build/Debug+Asserts/lib
+add_bin $LLVM_SOURCE/build/bin
+add_lib $LLVM_SOURCE/build/lib
