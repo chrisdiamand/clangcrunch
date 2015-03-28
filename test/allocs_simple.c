da01545e32643799ce0d98b3d577a10266fa890a
Chris Diamand <chris@diamand.org>
2015-03-28 12:45:53 +0000

    Use test struct to stop it being stripped from debug info.

--- test/allocs_simple.c
+++ test/allocs_simple.c
@@ -12,12 +12,15 @@ int main(void)
 {
     void *b = calloc(1, sizeof(struct foo));
 
-    // assert that the alloc is a blah
+    // assert that the alloc is a struct foo
     struct uniqtype *got_type = __liballocs_get_alloc_type(b);
     struct uniqtype *data_type = dlsym(RTLD_NEXT, "__uniqtype__foo");
     assert(data_type);
     assert(got_type);
     assert(got_type == data_type);
+
+    // We need to use the struct or clang will remove the unused debug info.
+    ((struct foo *) b)->x = 3;
 
     return 0;
 }
