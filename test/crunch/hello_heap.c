069d36b602724a1848fe037c22e00606a8f5a1a2
Chris Diamand <chris@diamand.org>
2015-03-29 22:59:25 +0100

    Tidy up hello_heap test.

--- test/crunch/hello_heap.c
+++ test/crunch/hello_heap.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 
-int is_a(void *obj, const char *typestr);
+int main() {
+    int *blah = (int *) malloc(200 * sizeof(int));
 
-int main()
-{
-	int *blah = (int *) malloc(200 * sizeof (int));
-	for (int i = 0; i < 200; ++i) blah[i] = 42;
-	
-	void *fake = blah;
+    for (int i = 0; i < 200; ++i)
+        blah[i] = 42;
 
-	int *recovered = (int *) fake;
+    void *fake = blah;
+    int *recovered = (int *) fake;
 
-	printf("It says: %d\n", recovered[0]);
+    printf("It says: %d\n", recovered[0]);
 
-	free(blah);
-	
-	return 0;
+    free(blah);
+    return 0;
 }
