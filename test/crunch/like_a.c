973f69c1e279565155adbfbf2e24900dbb5a475a
Chris Diamand <chris@diamand.org>
2015-04-01 18:14:10 +0100

    Fix build of 'void' and 'like_a' tests.
    Define _GNU_SOURCE in the source file, since it seems to be disappearing from
    the command-line somewhere.

--- test/crunch/like_a.c
+++ test/crunch/like_a.c
@@ -1,3 +1,7 @@
+#ifndef _GNU_SOURCE
+    #define _GNU_SOURCE
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -12,14 +16,14 @@ struct sockaddr test_sockaddr;
 
 int main(void)
 {
-	struct sockaddr_in *p_mysock = malloc(sizeof (struct sockaddr));
-	
-	fprintf(stderr, "Allocated a sockaddr_in at %p\n", p_mysock);
-	
-	const void *sockaddr_type = dlsym(RTLD_DEFAULT, "__uniqtype__sockaddr");
-	assert(sockaddr_type);
-	
-	assert(__like_aU(p_mysock, sockaddr_type));
+    struct sockaddr_in *p_mysock = malloc(sizeof (struct sockaddr));
 
-	return 0;
+    fprintf(stderr, "Allocated a sockaddr_in at %p\n", p_mysock);
+
+    const void *sockaddr_type = dlsym(RTLD_DEFAULT, "__uniqtype__sockaddr");
+    assert(sockaddr_type);
+
+    assert(__like_aU(p_mysock, sockaddr_type));
+
+    return 0;
 }
--- test/crunch/void.c
+++ test/crunch/void.c
@@ -1,16 +1,20 @@
+#ifndef _GNU_SOURCE
+    #define _GNU_SOURCE
+#endif
+
 #include <stdio.h>
 #include <assert.h>
 #include "libcrunch.h"
 
 int main(void)
 {
-	/* NOTE: the point of this test case is that libcrunch SHOULD 
-	 * be inited, because the strengthening of void is checked. */
-	struct foo {
-		int bar;
-	} *blah = malloc(sizeof (struct foo));
-	
-	assert(__libcrunch_is_initialized);
+    /* NOTE: the point of this test case is that libcrunch SHOULD
+     * be inited, because the strengthening of void is checked. */
+    struct foo {
+        int bar;
+    } *blah = malloc(sizeof(struct foo));
 
-	return 0;
+    assert(__libcrunch_is_initialized);
+
+    return 0;
 }
