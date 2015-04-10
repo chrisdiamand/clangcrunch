b283218bc2560ca3b2ec942e35ff08adc4fd1151
Chris Diamand <chris@diamand.org>
2015-04-10 21:49:26 +0100

    Remove unused example-test-things.

--- test/allocs.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-
-int main(void) {
-    int *x = malloc(sizeof(int) * 3);
-    long *lp = (long *) x;
-    return 0;
-}
--- test/is_aU.c
+++
@@ -1,7 +0,0 @@
-int __is_aU(void *, const char *);
-
-int main(void) {
-    int *x;
-    __is_aU(x, "int32");
-    return 0;
-}
